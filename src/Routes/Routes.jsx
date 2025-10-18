00ba495c70fbb7d815d7ca9ab68e84087d480d5f
Badsha012 <badshasheikh361@gmail.com>
2025-10-18 10:50:00 +0600

    five commit

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,23 +1,24 @@
 import { createBrowserRouter } from "react-router";
+import HomeLayout from "../Layouts/HomeLayout";
 
 const router= createBrowserRouter(
 
     [
         {
             path:'/',
-            element:<h2>Home Layout</h2>,
+            element:<HomeLayout></HomeLayout>,
         },
         {
             path:'/auth',
-            element:<h2>Authentication Layout</h2>
+            element:<h2>Authentication Layout</h2>,
         },
         {
             path:'/news',
-            element:<h1>News Layout</h1>
+            element:<h1>News Layout</h1>,
         },
         {
             path:'/*',
-            element:<h2>Error Message</h2>
+            element:<h2>Error Message</h2>,
         }
     ]
 )
