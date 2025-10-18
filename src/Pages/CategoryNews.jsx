ffccc23777d531e70bada11b7cb1dcfdd17c44a6
Badsha012 <badshasheikh361@gmail.com>
2025-10-18 21:45:29 +0600

    second commit

--- src/Pages/CategoryNews.jsx
+++ src/Pages/CategoryNews.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
-import { useParams } from 'react-router';
+
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
 
-    const { id } =useParams()
-    console.log(id);
+    const { id } =useParams();
+    const data=useLoaderData();
+
+    const [categoryNews,setcatagoryNews]=useState([]);
+    //console.log(data);
+   // console.log(id);
+   useEffect(()=>{
+    if(id=="0"){
+     setcatagoryNews(data) ; 
+     return ; 
+    }
+    else if(id=="1"){
+          const filterNews=data.filter(news=>news.other.is_today_pick == true);
+          setcatagoryNews(filterNews)
+   
+
+    }
+    else{
+
+         const filterNews=data.filter(news=>news.category_id == id);
+   console.log(filterNews);
+    setcatagoryNews(filterNews)
+
+   }
+    }
+  ,[data,id])
     return (
         <div>
-            category news - {id}
-        </div>
+          Total {categoryNews.length 
+        } news Found       </div>
     );
 };
 
